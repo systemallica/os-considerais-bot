3d5c867afa4c0252c09d813d9b219a39f2532c9c
Andrés Reverón Molina <andressser@gmail.com>
2020-06-30 10:34:18 +0200

    initial commit

---
+++ handler.py
@@ -0,0 +1,89 @@
+import json
+import telegram
+import os
+import logging
+from pathlib import Path
+from random import randrange
+
+
+# Logging is cool!
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+OK_RESPONSE = {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': json.dumps('ok')
+}
+ERROR_RESPONSE = {
+    'statusCode': 400,
+    'body': json.dumps('Oops, something went wrong!')
+}
+
+path = Path(__file__).parent / "../raw/filtered_words.txt"
+words_file = open(path, 'r')
+words = words_file.readlines()
+
+
+def configure_telegram():
+    """
+    Configures the bot with a Telegram Token.
+    Returns a bot instance.
+    """
+
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    if not TELEGRAM_TOKEN:
+        logger.error('The TELEGRAM_TOKEN must be set')
+        raise NotImplementedError
+
+    return telegram.Bot(TELEGRAM_TOKEN)
+
+
+def webhook(event, context):
+    """
+    Runs the Telegram webhook.
+    """
+
+    bot = configure_telegram()
+    logger.info('Event: {}'.format(event))
+
+    if event.get('httpMethod') == 'POST' and event.get('body'):
+        logger.info('Message received')
+        update = telegram.Update.de_json(json.loads(event.get('body')), bot)
+        chat_id = update.message.chat.id
+        text = update.message.text
+
+        if text == '/start':
+            text = "Hello!"
+        elif text == "/consideracion":
+            word = words[randrange(len(words))]
+            text = "¿Os consideráis " + word.strip() + "?"
+
+        bot.sendMessage(chat_id=chat_id, text=text)
+        logger.info('Message sent')
+
+        return OK_RESPONSE
+
+    return ERROR_RESPONSE
+
+
+def set_webhook(event, context):
+    """
+    Sets the Telegram bot webhook.
+    """
+
+    logger.info('Event: {}'.format(event))
+    bot = configure_telegram()
+    url = 'https://{}/{}/'.format(
+        event.get('headers').get('Host'),
+        event.get('requestContext').get('stage'),
+    )
+    webhook = bot.set_webhook(url)
+
+    if webhook:
+        return OK_RESPONSE
+
+    return ERROR_RESPONSE
---
+++ word_filter.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+path = Path(__file__).parent / "../raw/words.txt"
+words_file = open(path, 'r')
+words = words_file.readlines()
+
+filtered_words = []
+print("Original dictionary length:", len(words))
+
+for word in words:
+    word = word.replace("á", "a", -1).replace("é", "e", -1).replace("í", "i", -1).replace("ó", "o", -1).replace("ú", "u", -1)
+    if word.strip().endswith(("er", "ir", "ar", "-", "arse", "mente")) or word.strip().startswith("-") or word.find(" ") > -1:
+        continue
+    elif word.strip().endswith(("a", "e", "o")):
+        word = word.strip() + "s\n"
+    elif word.strip().endswith(("ad", "al", "el", "ol", "on", "u", "i")):
+        word = word.strip() + "es\n"
+    elif word.strip().endswith("ez"):
+        word = word.strip()[:-1] + "ces\n"
+    filtered_words.append(word)
+
+print("Filtered dictionary length:", len(filtered_words))
+
+path = Path(__file__).parent / "./raw/filtered_words.txt"
+with open(path, "w") as txt_file:
+    for word in filtered_words:
+        txt_file.write(word)
