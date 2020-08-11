43388e97371cdc2b765d02c2267467c1fa75c9e2
Andrés Reverón Molina <andressser@gmail.com>
2020-08-11 12:17:26 +0200

    updates to dictionary algorithms

--- utils/create_singulars.py
+++ utils/create_singulars.py
@@ -11,17 +11,12 @@ for word in words:
     word = word.replace("á", "a", -1).replace("é", "e", -1).replace("í", "i", -1).replace("ó", "o", -1).replace("ú", "u", -1)
     if word.strip().endswith(("er", "ir", "ar", "-", "arse", "mente")) or word.strip().startswith("-") or word.find(" ") > -1:
         continue
-    elif word.strip().endswith(("a", "e", "o")):
-        word = word.strip() + "s\n"
-    elif word.strip().endswith(("ad", "al", "el", "ol", "il", "or", "in", "on", "u", "i")):
-        word = word.strip() + "es\n"
-    elif word.strip().endswith(("iz", "ez")):
-        word = word.strip()[:-1] + "ces\n"
-    filtered_words.append(word)
+    elif word.strip().endswith(("al", "able", "ible", "ador", "oro", "ito", "sio", "iso", "imo", "ivo", "ense", "eso", "aco", "ado", "ante", "orio")):
+        filtered_words.append(word)
 
-print("Filtered dictionary length:", len(filtered_words))
+print("Singular dictionary length:", len(filtered_words))
 
-path = Path(__file__).parent / "../raw/filtered_words.txt"
+path = Path(__file__).parent / "../raw/singular_words.txt"
 with open(path, "w") as txt_file:
     for word in filtered_words:
         txt_file.write(word)
